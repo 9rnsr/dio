6e6d0bd08c1c1b1c57769a0349938d83aa6b2394
Kenji Hara <k.hara.pg@gmail.com>
2012-04-07 15:56:59 +0900

    Add io.socket and TcpSocket device

---
+++ io/socket.d
@@ -0,0 +1,106 @@
+module io.socket;
+
+import std.socket;
+import std.typecons : scoped;
+
+struct TcpSocket
+{
+    std.socket.TcpSocket socket;
+    size_t* pRefCounter;
+
+    /**
+    */
+    this(string hostname, ushort port)
+    {
+        auto ih = scoped!InternetHost();
+        if (!ih.getHostByName(hostname))
+            throw new HostException("unresolved host name");
+        this(new InternetAddress(ih.addrList[$-1], port));
+
+        pRefCounter = new size_t;
+        *pRefCounter = 1;
+    }
+    /**
+    */
+    this(InternetAddress iaddr)
+    {
+        socket = new std.socket.TcpSocket(iaddr);
+        socket.blocking = false;
+    }
+    this(this)
+    {
+        if (pRefCounter)
+            ++(*pRefCounter);
+    }
+    ~this()
+    {
+        if (pRefCounter)
+        {
+            if (--(*pRefCounter) == 0)
+            {
+                if (socket)
+                    socket.close();
+            }
+        }
+    }
+
+    /**
+    */
+    bool pull(ref ubyte[] buf)
+    {
+        if (buf.length == 0)
+            return false;
+
+        sizediff_t n = socket.receive(buf[]);
+        if (n < 0)
+            return wouldHaveBlocked() ? true : false;
+        if (n == 0)
+            return false;
+        assert(n <= buf.length);
+        buf = buf[n .. $];
+        return true;
+    }
+
+    /**
+    */
+    bool push(ref const(ubyte)[] buf)
+    {
+        if (buf.length == 0)
+            return false;
+
+        sizediff_t n = socket.send(buf);
+        if (n < 0)
+            return wouldHaveBlocked() ? true : false;
+        assert(n <= buf.length);
+        buf = buf[n .. $];
+        return true;
+    }
+}
+
+unittest
+{
+    auto sock = TcpSocket("www.digitalmars.com", 80);
+    const(ubyte)[] sendbuf = cast(const(ubyte)[])"GET / HTTP/1.0\r\n\r\n";
+    while (sock.push(sendbuf)){}
+
+    ubyte[4096] buffer;
+    ubyte[] recvbuf = buffer[];
+    while (sock.pull(recvbuf)) {}
+    recvbuf = buffer[0 .. $-recvbuf.length];
+}
+
+version(unittest)
+{
+    import io.core;
+    import io.socket;
+    import std.array : array;
+}
+unittest
+{
+    auto sock = TcpSocket("dlang.org", 80);
+    auto wr = sock.sinked /*.buffered*/.coerced!char.ranged;
+    auto rd = sock.sourced  .buffered  .coerced!char.ranged;
+
+    wr.put("GET / HTTP/1.0\r\n\r\n");
+    auto recvbuf = rd.array();
+}
